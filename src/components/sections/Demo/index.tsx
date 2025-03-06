ae0cc1500bdd19eb8eabe734db71b1fe8ec0cc78
vacabernardo <vacabernardo@users.noreply.github.com>
2025-03-06 08:01:48 -0600

    1

---
+++ src/components/sections/Demo/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import MuiBox from '@mui/material/Box';
+import MuiTypography from '@mui/material/Typography';
+
+type Props = {
+    title: string;
+    description: string;
+};
+
+export const Demo: React.FC<Props> = (props) => {
+    const { title, description } = props;
+
+    return (
+        <MuiBox sx={{ py: { xs: 6, sm: 10 } }}>
+            <MuiTypography component="h2" variant="h2" color="text.primary">
+                {title}
+            </MuiTypography>
+            <MuiTypography component="p" variant="body1" color="text.secondary" sx={{ mt: 2 }}>
+                {description}
+            </MuiTypography>
+        </MuiBox>
+    );
+};
